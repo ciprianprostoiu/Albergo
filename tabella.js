1ec8aaeddb8e21bf0e40eec282e62a964a28f3c3
tommaso <ciolacutommaso@itis-molinari.eu>
2024-10-18 11:37:49 +0200

    ora la form riesce ad aggiungere elementi alla table

--- tabella.js
+++ tabella.js
@@ -3,22 +3,35 @@ const tableDiv = document.getElementById("table");
 
 
 const creaTable = (parentElement) => {
-  let data;
+  let data = [];
   return {
-    build: (dataInput) => {
+    setData: (dataInput) => {
       data = dataInput;
     },
-    render: () => {
+    render: () => { // problema nella generazione della tabella
       let html = `<table class="table m-3 table-bordered">`;
-      html += data.map((row) => "<tr>" + row.map((col) => "<td>" + col + "</td>"
-        ).join("")
-      ).join("") + "</tr>";
-      html += "</table";
+      data.forEach((row) => {
+        html += "<tr>" + 
+        "<td>" + row.data + "</td>" + 
+        "<td>" + row.singole + "</td>" + 
+        "<td>" + row.multiple + "</td>" + 
+        "<td>" + row.suite + "</td>" + 
+        "</tr>";
+      })
+      html += "</table>";
       parentElement.innerHTML = html;
+    },
+    addData: (dataInput) => {
+      data.push(dataInput);
     }
   }
 }
 
-const table1 = creaTable(tableDiv);
-table1.build([["Data", "Singola", "Multiple", "Suite"]]);
-table1.render();
+const table1 = creaTable(tableDiv); // i due componenti non riescono a comunicare correttamente
+table1.addData([{data: "Data", singole: "Singole", multiple: "Multiple", suite: "Suite"}]);
+//download().then((r) => r.json).then((r) => {table1.setData(r); table1.render();})
+table1.render()
+
+
+
+
